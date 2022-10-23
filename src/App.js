e347e0da68e7ae917aa7a39ac7537f29cac7ab98
Tomonori Shimomura <shimomut@amazon.com>
2022-10-22 20:08:19 -0700

    Added authentication UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Amplify related
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify, Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>
+          <a className='App-Link' href="." onClick={Auth.signOut}>Sign out</a>
+        </h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,4 +33,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
