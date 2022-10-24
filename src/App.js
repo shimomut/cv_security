c05b2ada908c276d8000e591d16a18baec65109b
Tomonori Shimomura <shimomut@amazon.com>
2022-10-24 11:08:48 -0700

    Added DataStore usage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,126 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-// Amplify related
 import '@aws-amplify/ui-react/styles.css';
-import { Amplify, Auth } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
 
+import { useState, useEffect } from "react";
+import { Auth } from 'aws-amplify';
+import { DataStore, Predicates, SortDirection } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+import { 
+  HeaderComponent,
+  BoardComponent,
+  //BoardComponentCollection,
+  PersonComponentCollection 
+} from "./ui-components"
+
+import { Board } from './models';
+
+//---
+
+//const content1 = <p>タブ1のコンテンツ</p>
+//const content1 = <BoardComponentCollection></BoardComponentCollection>
+
+//const content2 = <p>タブ2のコンテンツ</p>
+const content2 = <PersonComponentCollection></PersonComponentCollection>
+
+const content3 = <p>タブ3のコンテンツ</p>
+const content4 = <p>タブ4のコンテンツ</p>
+
+function filterAndSort( input, setContent1, doChange ) {
+  DataStore.query(Board, Predicates.ALL, {
+    sort: ob => ob.name(SortDirection.ASCENDING).createdAt( SortDirection.DESCENDING ),
+    page: +input,
+    limit: 3
+  }).then(
+    values => {
+      const data = []
+      for( let item of values ){
+        data.push(
+          <BoardComponent board={item} key={item.id}>
+          </BoardComponent>
+        )
+      }      
+      setContent1(
+        <div>
+          <input type="number" className='my-2 form-control' onChange={doChange} ></input>
+          {data}
+        </div>
+      )
+    }
+  );
+}
+
+function updateCreateTab() {
+}
 
 function App() {
+
+  const [content1, setContent1] = useState("");
+  const [input, setInput] = useState(0);
+  //const [find, setFind] = useState(input);
+
+  function doChange(e) {
+    setInput( +e.target.value );
+  }
+
+  /*
+  function doFilter(e) {
+    setFind( input );
+  }
+  */
+
+  useEffect(
+    () => {
+      filterAndSort( input, setContent1, doChange );
+    }, [input]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>
-          <a className='App-Link' href="." onClick={Auth.signOut}>Sign out</a>
-        </h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <HeaderComponent className="my-4"/>
+      <p>これはUI Componentを使った表示です</p>
+
+      <ul className='nav nav-tabs'>
+        <li className='nav-item'>
+          <a href="#tab1" className="nav-link active" data-bs-toggle="tab">List</a>
+        </li>
+        <li className='nav-item'>
+          <a href="#tab2" className="nav-link" data-bs-toggle="tab">Create</a>
+        </li>
+        <li className='nav-item'>
+          <a href="#tab3" className="nav-link" data-bs-toggle="tab">Update</a>
+        </li>
+        <li className='nav-item'>
+          <a href="#tab4" className="nav-link" data-bs-toggle="tab">Delete</a>
+        </li>
+      </ul>
+
+      <div className="tab-content">
+        <div id="tab1" className='my-2 tab-pane active'>
+          {content1}
+        </div>
+        <div id="tab2" className='my-2 tab-pane'>
+          {content2}
+        </div>
+        <div id="tab3" className='my-2 tab-pane'>
+          {content3}
+        </div>
+        <div id="tab4" className='my-2 tab-pane'>
+          {content4}
+        </div>
+      </div>
+
+      <p>
+        <a className='btn btn-primary' href="." onClick={Auth.signOut}>
+          Sign Out
         </a>
-      </header>
+      </p>
+
     </div>
   );
 }
+
 
 export default withAuthenticator(App);
