b09ff0aefe65b1fcd2ce681e486b9ff3ad16ca38
Tomonori Shimomura <shimomut@amazon.com>
2022-10-27 03:33:29 +0000

    Use AWS NorthStar

--- src/Other.js
+++ src/Other.js
@@ -1,29 +1,35 @@
 import { useState, useEffect } from "react";
 
-import '@aws-amplify/ui-react/styles.css';
+//import '@aws-amplify/ui-react/styles.css';
 
-import { 
+import {
     //Auth, 
-    Storage 
+    Storage,
+    API,
 } from 'aws-amplify';
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
+//import { withAuthenticator } from '@aws-amplify/ui-react';
 
+/*
 import { 
   HeaderComponent,
   //BoardComponent,
   //BoardComponentCollection,
   //PersonComponentCollection 
 } from "./ui-components"
+*/
 
-import { 
-    BrowserRouter, 
+import NorthStarThemeProvider from 'aws-northstar/components/NorthStarThemeProvider';
+import Button from 'aws-northstar/components/Button';
+
+import {
+    BrowserRouter,
     Route,
-    Routes,
+    Switch,
     Link,
 } from 'react-router-dom';
 
-function HomePage(props){
+function HomePage(props) {
     return (
         <div>
             <p>This is home.</p>
@@ -31,7 +37,7 @@ function HomePage(props){
     );
 }
 
-function ListPage(props){
+function ListPage(props) {
     return (
         <div>
             <p>This is List page.</p>
@@ -39,7 +45,7 @@ function ListPage(props){
     );
 }
 
-function AboutPage(props){
+function AboutPage(props) {
     return (
         <div>
             <p>This is About page.</p>
@@ -47,7 +53,7 @@ function AboutPage(props){
     );
 }
 
-function NotFoundPage(props){
+function NotFoundPage(props) {
     return (
         <div>
             <p>This is NotFound page.</p>
@@ -55,36 +61,115 @@ function NotFoundPage(props){
     );
 }
 
-function Other() {
+function GateEventComponent(props) {
 
-    const home_page = <HomePage/>
+    const [image, setImage] = useState("");
+
+    var message = "Not tailgating";
+    if (props.item.security_insights.tailgating) {
+        message = "Tailgating";
+    }
+
+    const opt = {
+        level: "public"
+    };
+    Storage.get(props.item.image, opt).then(
+        value => {
+            setImage(
+                <img width="300px" src={value}></img>
+            );
+        }
+    ).catch(
+        err => {
+            setImage(
+                ""
+            );
+        }
+    );
 
     return (
-        <BrowserRouter>
-            <h1>Router test</h1>
-
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/list">List</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
-
-            <Routes>
-                <Route path="/" element={home_page} />
-                <Route path="/list" element={<ListPage/>} />
-                <Route path="/about" element={<AboutPage/>} />
-                <Route path="*" element={<NotFoundPage/>} />
-            </Routes>
-        </BrowserRouter>
-  
-
+        <div className='border border-primary px-3 py-2'>
+            <p>{props.item.camera}</p>
+            <p>{props.item.timestamp}</p>
+            <p>{message}</p>
+            {image}
+        </div>
     );
 }
 
-export default withAuthenticator(Other);
+function updateGateEvents( setContent ){
+}
+
+function GateEventListPage(){
+
+    const [content, setContent] = useState("");
+
+    useEffect(
+        () => {
+            API.get("api60ee79cf", "/gateevent").then(
+
+                response => {
+        
+                    console.log(response);
+        
+                    const components = []
+                    for (let item of response) {
+                        components.push(
+                            <GateEventComponent item={item} key={item.camera + item.timestamp}></GateEventComponent>
+                        );
+                    }
+                
+                    setContent( components )
+                }
+            ).catch(
+                err => {
+                    console.log(err)
+                }
+            );        
+        },
+        []
+    );
+
+    return (
+        <div>
+            {content}
+        </div>
+    );
+}
+
+function Other() {
+
+    return (
+        <NorthStarThemeProvider>
+            <BrowserRouter>
+                <h1>Menu</h1>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/list">List</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+
+                <h1>Contents</h1>
+
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/list" component={GateEventListPage} />
+                    <Route exact path="/about" component={AboutPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+
+                <Button variant="primary">Button test</Button>
+
+            </BrowserRouter>
+        </NorthStarThemeProvider>
+    );
+}
+
+//export default withAuthenticator(Other);
+export default Other;
